45eac9882631766366b3adb1bb6baa4f07ccb6e1
Ahmad Tuflihun <ahmadtuflihunxd@gmail.com>
2023-10-06 23:52:14 +0700

    Update damage system

---
+++ Assets/_MainAssets/Modules/DamageSystem/DamagePopupComponent.cs
@@ -0,0 +1,59 @@
+﻿using DG.Tweening;
+using TMPro;
+using UnityEngine;
+
+namespace DamageSystem
+{
+    public class DamagePopupComponent : MonoBehaviour
+    {
+        [SerializeField] private TMP_Text damageText;
+        [SerializeField] private Color damageColor = Color.red;
+        [SerializeField] private int damageFontSize = 20;
+        [SerializeField] private float damageDuration = 0.5f;
+        
+        private Canvas canvas;
+
+        private void Awake()
+        {
+            Init();
+        }
+
+        private void Update()
+        {
+            if (!Input.GetKeyDown(KeyCode.R)) return;
+
+            var quad = GameObject.Find("Quad").transform.position;
+            var quadPos = Camera.main.WorldToScreenPoint(quad);
+
+            var targetPosition = Input.mousePosition;
+            ShowDamage(10, quadPos, new Vector2(0, 10));
+        }
+
+
+        private void Init()
+        {
+            canvas = damageText.canvas;
+        }
+
+        public void ShowDamage(int damage, Vector2 targetPos, Vector2 offset = default)
+        {
+            targetPos.x -= (float)Screen.width / 2;
+            targetPos.y -= (float)Screen.height / 2;
+            targetPos += offset;
+            targetPos /= canvas.scaleFactor;
+
+
+            damageText.text = damage.ToString();
+            damageText.color = damageColor;
+            damageText.fontSize = damageFontSize;
+            damageText.enabled = true;
+            // Reset Position and Alpha
+            damageText.rectTransform.anchoredPosition = targetPos;
+            damageText.alpha = 1f;
+            // Move and Fade
+            damageText.rectTransform.DOLocalMoveY(damageText.rectTransform.localPosition.y + 10, damageDuration)
+                .SetEase(Ease.OutQuad);
+            damageText.DOFade(0f, damageDuration).SetEase(Ease.OutQuad).OnComplete(() => damageText.enabled = false);
+        }
+    }
+}
---
+++ Assets/_MainAssets/Modules/Leveling/BaseLevel.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using UnityEngine;
+
+namespace Leveling
+{
+    [Serializable]
+    public class BaseLevel
+    {
+        public int Level;
+        public int Exp;
+        public int MaxExp;
+        public int MaxLevel;
+        public int Ascension;
+        public int MaxAscension;
+        public int Refinement;
+        public int MaxRefinement;
+
+        public BaseLevel(int level, int exp, int maxExp, int maxLevel, int ascension, int maxAscension, int refinement,
+            int maxRefinement)
+        {
+            Level = level;
+            Exp = exp;
+            MaxExp = maxExp;
+            MaxLevel = maxLevel;
+            Ascension = ascension;
+            MaxAscension = maxAscension;
+            Refinement = refinement;
+            MaxRefinement = maxRefinement;
+        }
+
+        public BaseLevel()
+        {
+            Level = 1;
+            Exp = 0;
+            MaxExp = 100;
+            MaxLevel = 100;
+            Ascension = 0;
+            MaxAscension = 10;
+            Refinement = 0;
+            MaxRefinement = 10;
+        }
+
+        public void AddExp(int exp)
+        {
+            Exp += exp;
+            if (Exp < MaxExp) return;
+            Exp -= MaxExp;
+            Level++;
+            if (Level < MaxLevel) return;
+            Level = MaxLevel;
+            Exp = 0;
+        }
+
+        public void AddAscension(int ascension)
+        {
+            Ascension += ascension;
+            if (Ascension < MaxAscension) return;
+            Ascension -= MaxAscension;
+            Refinement++;
+            if (Refinement < MaxRefinement) return;
+            Refinement = MaxRefinement;
+            Ascension = 0;
+        }
+
+        public void AddRefinement(int refinement)
+        {
+            Refinement += refinement;
+            if (Refinement < MaxRefinement) return;
+            Refinement -= MaxRefinement;
+            Ascension++;
+            if (Ascension < MaxAscension) return;
+            Ascension = MaxAscension;
+            Refinement = 0;
+        }
+
+        public BaseLevel FromJson(string json)
+        {
+            return JsonUtility.FromJson<BaseLevel>(json);
+        }
+
+        public string ToJson()
+        {
+            return JsonUtility.ToJson(this);
+        }
+    }
+}
