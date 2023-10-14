59e408d3143f3f328e971c22079b213b8d9ed516
Ahmad Tuflihun <ahmadtuflihunxd@gmail.com>
2023-10-14 14:54:25 +0700

    GAQ-3 test commit

--- Assets/_MainAssets/Modules/DamageSystem/ObjectController.cs
+++ Assets/_MainAssets/Modules/DamageSystem/ObjectController.cs
@@ -1,13 +1,13 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 namespace DamageSystem
 {
     public class ObjectController : MonoBehaviour, IDamageable, IHealth
     {
-        private void Start()
-        {
-            Init(50);
-        }
+        public int Health { get; set; }
+        public int MaxHealth { get; }
+        public Action OnDeathEvent { get; set; }
 
         public void Init(int maxHealth)
         {
@@ -20,12 +20,10 @@ namespace DamageSystem
             if (Health <= 0) OnDeath();
         }
 
+
         public void OnDeath()
         {
-            Destroy(gameObject);
+            OnDeathEvent?.Invoke();
         }
-
-        public int Health { get; set; }
-        public int MaxHealth { get; }
     }
 }
